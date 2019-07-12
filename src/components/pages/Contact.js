3c91247911d77e47c792e02ace113d72969c6d38
Taylor Short <treddshort@gmail.com>
2019-07-12 08:33:08 -0700

    first commit

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Contact = () => {
+  return (
+    <Fragment>
+      <h1>Contact us:</h1>
+      <p>P: (425) 552-5577</p>
+      <a href="mailto:treddshort@gmail.com">Email us!</a>
+    </Fragment>
+  );
+};
+
+export default Contact;
